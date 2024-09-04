790f0a9ab375b2c1c589d1f2efb64f6792cacc54
Mangaljeet Pandey <mangaljeet.pandey@databell.in>
2024-09-04 15:50:37 +0530

    added new features

--- src/Emailer.js
+++ src/Emailer.js
@@ -8,10 +8,15 @@ import {
   ProgressBar,
   Card,
   Alert,
+  Modal,
+  Tooltip,
+  OverlayTrigger,
 } from "react-bootstrap";
+import { FiSettings } from "react-icons/fi";
 import Papa from "papaparse";
 import axios from "axios";
 import "./BulkEmailer.css";
+import img from "./img.png";
 
 const BulkEmailer = () => {
   const [csvData, setCsvData] = useState([]);
@@ -21,11 +26,15 @@ const BulkEmailer = () => {
   const [sending, setSending] = useState(false);
   const [error, setError] = useState("");
   const [alert, setAlert] = useState("");
+  const [showConfigModal, setShowConfigModal] = useState(false);
+  const [showInstructionsModal, setShowInstructionsModal] = useState(false);
+  const [appEmail, setAppEmail] = useState("");
+  const [appPassword, setAppPassword] = useState("");
 
   const handleFileUpload = (e) => {
     const file = e.target.files[0];
-    if(!(file && file.name)){
-        return null
+    if (!(file && file.name)) {
+      return null;
     }
     const fileExtension = file.name.split(".").pop().toLowerCase();
     const allowedExtensions = ["csv", "xlsx", "xls"];
@@ -54,7 +63,7 @@ const BulkEmailer = () => {
 
   const handleSendEmails = async () => {
     if (csvData.length === 0) {
-      setError("Please provide csv data:)");
+      setError("Please provide CSV data.");
       return;
     }
     if (!emailSubject.trim() || !emailBody.trim()) {
@@ -76,6 +85,8 @@ const BulkEmailer = () => {
           bcc: bcc ? bcc.split(",") : [],
           subject: `${emailSubject}`,
           text: body,
+          appEmail,
+          appPassword,
         });
         setProgress(Math.round(((i + 1) / totalEmails) * 100));
       }
@@ -86,19 +97,58 @@ const BulkEmailer = () => {
     }
   };
 
+  const handleConfigSave = () => {
+    if (appEmail && appPassword) {
+      setShowConfigModal(false);
+    } else {
+      setAlert("Both App Email and App Password are required.");
+    }
+  };
+
   return (
     <Container className="mt-5">
       <Row className="justify-content-md-center">
         <Col md={8}>
           <Card className="shadow-sm">
             <Card.Body>
-              <h2 className="text-center mb-4">Bulk Emailer</h2>
+              <div className="d-flex justify-content-between align-items-center mb-4">
+                {/* <h2 className="text-center">Bulk Emailer</h2> */}
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={
+                    <Tooltip id="settings-tooltip">
+                      Set up your configuration for bulk emailing. We do not
+                      store any information in the database; it's real-time data
+                      that will be lost when you refresh the page.
+                    </Tooltip>
+                  }
+                >
+                  <Button
+                    variant="link"
+                    onClick={() => setShowConfigModal(true)}
+                    className="p-0"
+                  >
+                    <FiSettings size={24} />
+                  </Button>
+                </OverlayTrigger>
+                <Button
+                  variant="danger"
+                  className="mb-3"
+                  onClick={() => setShowInstructionsModal(true)}
+                >
+                  *You Have to Read These Instructions Before Using this
+                  application.
+                </Button>
+              </div>
+
+              <h2 className="text-center">Bulk Emailer</h2>
+
               <Form>
                 {error && <Alert variant="danger">{error}</Alert>}
                 {alert && <Alert variant="warning">{alert}</Alert>}
 
                 <Form.Group controlId="formFile" className="mb-4">
-                  {/* <Form.Label>Import CSV, XLSX, or XLS</Form.Label> */}
+                  <Form.Label>Upload CSV File</Form.Label>
                   <Form.Control
                     type="file"
                     accept=".csv,.xlsx,.xls"
@@ -119,17 +169,6 @@ const BulkEmailer = () => {
                 </Form.Group>
 
                 <Form.Group controlId="formBody" className="mb-4">
-                  <Form.Label
-                    style={{
-                      fontWeight: "bold",
-                      color: "#ff0000",
-                      fontSize: "14px",
-                    }}
-                  >
-                    {
-                      "Use [name] for dynamic value, like Hi [name] -> Hi Shivam"
-                    }
-                  </Form.Label>
                   <Form.Label>Email Body</Form.Label>
                   <Form.Control
                     as="textarea"
@@ -144,7 +183,7 @@ const BulkEmailer = () => {
                 <Button
                   variant="primary"
                   onClick={handleSendEmails}
-                  disabled={sending}
+                  disabled={sending || !appEmail || !appPassword}
                   className="w-100"
                 >
                   {sending ? "Sending Emails..." : "Send Emails"}
@@ -163,6 +202,105 @@ const BulkEmailer = () => {
           </Card>
         </Col>
       </Row>
+
+      <Modal show={showConfigModal} onHide={() => setShowConfigModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>App Configuration</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formAppEmail">
+              <Form.Label>App Email</Form.Label>
+              <Form.Control
+                type="email"
+                value={appEmail}
+                onChange={(e) => setAppEmail(e.target.value)}
+                placeholder="Enter app email"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formAppPassword" className="mt-3">
+              <Form.Label>App Password</Form.Label>
+              <Form.Control
+                type="password"
+                value={appPassword}
+                onChange={(e) => setAppPassword(e.target.value)}
+                placeholder="Enter app password"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowConfigModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleConfigSave}>
+            Save Config
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal
+        show={showInstructionsModal}
+        onHide={() => setShowInstructionsModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Instructions</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ol>
+            <li style={{ color: "red" }}>
+              You have to set up your app config. Simply click on the settings
+              icon to set it up. This configuration will not be stored in the
+              database; we manage it in real-time. When you refresh the page, it
+              will be automatically cleared.
+            </li>
+            <br />
+            <li style={{ color: "red" }}>
+              How can you get the app email and app password? Watch this video:{" "}
+              <br />
+              <a
+                href="https://youtu.be/MkLX85XU5rU?si=6OySCHpmQNjo4kVK"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://youtu.be/MkLX85XU5rU?si=6OySCHpmQNjo4kVK
+              </a>
+            </li>
+            <br />
+            <li style={{ color: "red" }}>
+              Here is the required CSV format. You need to scrape all data
+              following this format.
+              <img style={{ height: "100%", width: "100%" }} src={img}></img>
+            </li>
+            <br />
+            <li style={{ color: "red" }}>
+              You must first upload the CSV; after that, you can create the
+              subject.
+            </li>
+            <br />
+            <li style={{ color: "red" }}>
+              In the body, you can dynamically insert the CSV name using the
+              format [name]. For example:
+              <pre style={{ color: "black" }}>
+                Hi [name],
+                <br />I am reaching out to you to offer my product. If you're
+                interested, please check this out.
+              </pre>
+              The placeholder [name] will be replaced with each name from the
+              CSV.
+            </li>
+          </ol>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => setShowInstructionsModal(false)}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
